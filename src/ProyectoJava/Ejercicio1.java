15ee4561bd054cf68518a3cd136224979b406ec5
Ruben <rgbarco@alumnos.imf.com>
2021-02-05 10:49:02 +0100

    Es mi primera version

---
+++ src/ProyectoJava/Ejercicio1.java
@@ -0,0 +1,17 @@
+package ProyectoJava;
+
+public class Ejercicio1 {
+
+	public static void main(String[] args) {
+		// QUE FEO QUE ES EL CABRON DE JORGE, DE VERDAD QUE NO LO ENTIENDO, MENUDA TULA DE 2 CM QUE SE CALZA
+		
+		System.out.println("El samu sin embargo presume de una tula de 2,5");
+		System.out.println("Pero en contraparte ha contribuido a un bien mayor y hemos logrado sacar una humilde palmera de chocolate");
+		System.out.println("En base a brutales embestidas de simios subdesarrollados con problemas de irastibilidad");
+		
+		
+		System.out.println("No me gustan estas charlas, me ponen nervioso");
+		
+	}
+
+}
